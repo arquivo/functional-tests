c85533c39042badf5354427dd25ddc440f99976f
Vítor Gouveia <vitorgg@gmail.com>
2021-11-17 11:39:51 +0000

    Updated ImageSearchDirectUrlTest

--- src/test/java/pt/fccn/mobile/arquivo/tests/imagesearch/ImageSearchDirectUrlTest.java
+++ src/test/java/pt/fccn/mobile/arquivo/tests/imagesearch/ImageSearchDirectUrlTest.java
@@ -23,52 +23,51 @@ import pt.fccn.mobile.arquivo.utils.LocaleUtils;
  */
 public class ImageSearchDirectUrlTest extends WebDriverTestBaseParalell {
 
-	private static final String IMAGE_SEARCH_DIRECT_URL = "/images.jsp?size=all&type=&tools=off&safeSearch=on&query=fccn&btnSubmit=Search&dateStart=26%2F06%2F2007&dateEnd=27%2F06%2F2007";
+    private static final String IMAGE_SEARCH_DIRECT_URL = "/image/search?size=all&type=&tools=off&safeSearch=on&query=fccn&btnSubmit=Search&dateStart=26%2F06%2F2007&dateEnd=27%2F06%2F2007";
 
-	public ImageSearchDirectUrlTest(String os, String version, String browser, String deviceName,
-			String deviceOrientation) {
-		super(os, version, browser, deviceName, deviceOrientation);
-	}
+    public ImageSearchDirectUrlTest(String os, String version, String browser, String deviceName,
+            String deviceOrientation) {
+        super(os, version, browser, deviceName, deviceOrientation);
+    }
 
-	@Test
-	@Retry
-	public void imageSearchDirectUrlNoLanguageTest() throws Exception {
-		imageSearchDirectUrlTest(testURL + IMAGE_SEARCH_DIRECT_URL, Optional.empty());
-	}
+    @Test
+    @Retry
+    public void imageSearchDirectUrlNoLanguageTest() throws Exception {
+        imageSearchDirectUrlTest(testURL + IMAGE_SEARCH_DIRECT_URL, Optional.empty());
+    }
 
-	@Test
-	@Retry
-	public void imageSearchDirectUrlPTTest() throws Exception {
-		imageSearchDirectUrlTest(testURL + IMAGE_SEARCH_DIRECT_URL + "&" + LocaleUtils.languagePTUrlQueryParameter(),
-				Optional.of("Imagens"));
-	}
+    @Test
+    @Retry
+    public void imageSearchDirectUrlPTTest() throws Exception {
+    	imageSearchDirectUrlTest(testURL + IMAGE_SEARCH_DIRECT_URL + "&" + LocaleUtils.languagePTUrlQueryParameter(),
+    			Optional.of("Imagens"));
+    }
 
-	@Test
-	@Retry
-	public void imageSearchDirectUrlENTest() throws Exception {
-		imageSearchDirectUrlTest(testURL + IMAGE_SEARCH_DIRECT_URL + "&" + LocaleUtils.languageENUrlQueryParameter(),
-				Optional.of("Images"));
-	}
+    @Test
+    @Retry
+    public void imageSearchDirectUrlENTest() throws Exception {
+    	imageSearchDirectUrlTest(testURL + IMAGE_SEARCH_DIRECT_URL + "&" + LocaleUtils.languageENUrlQueryParameter(),
+    			Optional.of("Images"));
+    }
 
-	private void imageSearchDirectUrlTest(String url, Optional<String> imageButtonText) {
-		driver.get(url);
+    private void imageSearchDirectUrlTest(String url, Optional<String> imageButtonText) {
+        driver.get(url);
 
-		WebElement firstImage = waitUntilElementIsVisibleAndGet(By.id("imageResults0"));
-		assertNotNull("Should exist at least one image", firstImage);
+        WebElement firstImage = waitUntilElementIsVisibleAndGet(By.id("image-card-1"));
+        assertNotNull("Should exist at least one image", firstImage);
 
-		appendError(() -> {
-			List<WebElement> photoDivList = driver.findElements(By.xpath("//*[@id=\"photos\"]/div"));
-			assertEquals("Verify results count", 1, photoDivList.size());
-		});
+        appendError(() -> {
+            List<WebElement> photoDivList = driver.findElements(By.xpath("//*[@id=\"image-cards-container\"]/li"));
+            assertEquals("Verify results count", 24, photoDivList.size());
+        });
 
-		appendError(() -> assertThat("Check image original origin/domain",
-				driver.findElement(By.xpath("//*[@id=\"imageResults0\"]/p[1]")).getText(),
-				containsString("sag.fccn.pt")));
+        appendError(() -> assertThat("Check image original origin/domain",
+                driver.findElement(By.xpath("//*[@id=\"image-card-1\"]/ul/li[4]/a")).getText(),
+                containsString("fccn.pt")));
 
-		if (imageButtonText.isPresent()) {
-			appendError(() -> assertEquals("Check page language by verifying images button text", imageButtonText.get(),
-					driver.findElementById("ImageButton").getText()));
-		}
-	}
-
+        if (imageButtonText.isPresent()) {
+            appendError(() -> assertEquals("Check page language by verifying images button text", imageButtonText.get(),
+                    driver.findElementById("search-form-images").getText()));
+        }
+    }
 }
