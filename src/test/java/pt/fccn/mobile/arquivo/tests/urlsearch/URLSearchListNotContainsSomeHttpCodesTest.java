de645c44343aafb780be589584bea6c8370ae603
Vítor Gouveia <vitorgg@gmail.com>
2021-11-18 12:04:54 +0000

    Added urlsearch first class

--- src/test/java/pt/fccn/mobile/arquivo/tests/urlsearch/URLSearchListNotContainsSomeHttpCodesTest.java
+++ src/test/java/pt/fccn/mobile/arquivo/tests/urlsearch/URLSearchListNotContainsSomeHttpCodesTest.java
@@ -1,5 +1,7 @@
 package pt.fccn.mobile.arquivo.tests.urlsearch;
 
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertNotNull;
 
 import java.util.Arrays;
@@ -24,89 +26,92 @@ import pt.fccn.mobile.arquivo.utils.LocalizedString;
  */
 public class URLSearchListNotContainsSomeHttpCodesTest extends WebDriverTestBaseParalell {
 
-	public URLSearchListNotContainsSomeHttpCodesTest(String os, String version, String browser, String deviceName,
-			String deviceOrientation) {
-		super(os, version, browser, deviceName, deviceOrientation);
-	}
+    public URLSearchListNotContainsSomeHttpCodesTest(String os, String version, String browser, String deviceName,
+            String deviceOrientation) {
+        super(os, version, browser, deviceName, deviceOrientation);
+    }
 
-	@Test
-	@Retry
-	public void urlSearchListNotContains406PT() {
-		test("http://www.caleida.pt/saramago/", LocaleUtils.PORTUGUESE, Arrays.asList("19980205082901"),
-				Arrays.asList("20120131163447"));
-	}
+    @Test
+    @Retry
+    public void urlSearchListNotContains406PT() {
+        test("http://www.caleida.pt/saramago/", LocaleUtils.PORTUGUESE, Arrays.asList("list-results-timestamp-19980205082901"),
+                Arrays.asList("list-results-timestamp-20120131163447"));
+    }
 
-	@Test
-	@Retry
-	public void urlSearchListNotContains404EN() {
-		test("http://www.caleida.pt/saramago/", LocaleUtils.ENGLISH, Arrays.asList("20000413142115"),
-				Arrays.asList("20160210151550"));
-	}
+    @Test
+    @Retry
+    public void urlSearchListNotContains404EN() {
+        test("http://www.caleida.pt/saramago/", LocaleUtils.ENGLISH, Arrays.asList("list-results-timestamp-20000413142115"),
+                Arrays.asList("list-results-timestamp-20160210151550"));
+    }
 
-	@Test
-	@Retry
-	public void urlSearchListNotContains403PT() {
-		test("sapo.pt", LocaleUtils.PORTUGUESE, Arrays.asList("19971210144509"), Arrays.asList("20150424043204"));
-	}
+    @Test
+    @Retry
+    public void urlSearchListNotContains403PT() {
+        test("sapo.pt", LocaleUtils.PORTUGUESE, Arrays.asList("list-results-timestamp-19971210144509"),
+            Arrays.asList("list-results-timestamp-20150424043204"));
+    }
 
-	@Test
-	@Retry
-	public void urlSearchListNotContains503EN() {
-		test("record.pt", LocaleUtils.ENGLISH, Arrays.asList("19981202152653"), Arrays.asList("20171031183600"));
-	}
-	
-	//Verify if some redirect that is too close of a 200 isn't currently displayed on table/list of versions. Verify the missing of 302 but also verify if the corresponding 200 is displayed.
-	
-	@Test
-	@Retry
-	public void urlSearchListNotContains302PT() {
-		test("fccn.pt", LocaleUtils.PORTUGUESE, Arrays.asList("20161212184040"), Arrays.asList("20161213184117"));
-	}
+    @Test
+    @Retry
+    public void urlSearchListNotContains503EN() {
+        test("record.pt", LocaleUtils.ENGLISH, Arrays.asList("list-results-timestamp-19981202152653"),
+            Arrays.asList("list-results-timestamp-20171031183600"));
+    }
 
-	@Test
-	@Retry
-	public void urlSearchListNotContains302EN() {
-		test("fccn.pt", LocaleUtils.ENGLISH, Arrays.asList("20161212184040"), Arrays.asList("20161213184117"));
-	}
+    //Verify if some redirect that is too close of a 200 isn't currently displayed on table/list of versions. Verify the missing of 302 but also verify if the corresponding 200 is displayed.
 
-	private void test(String url, Locale locale, List<String> visibleVersions, List<String> invisibleVersions) {
+    @Test
+    @Retry
+    public void urlSearchListNotContains302PT() {
+        test("fccn.pt", LocaleUtils.PORTUGUESE, Arrays.asList("list-results-timestamp-20161212184040"),
+            Arrays.asList("list-results-timestamp-20161213184117"));
+    }
 
-		LocaleUtils.changeLanguageTo(this, locale);
+    @Test
+    @Retry
+    public void urlSearchListNotContains302EN() {
+        test("fccn.pt", LocaleUtils.ENGLISH, Arrays.asList("list-results-timestamp-20161212184040"),
+            Arrays.asList("list-results-timestamp-20161213184117"));
+    }
 
-		run("Search url", () -> {
-			driver.findElement(By.id("txtSearch")).clear();
-			driver.findElement(By.id("txtSearch")).sendKeys(url);
-			driver.findElement(By.xpath("//*[@id=\"buttonSearch\"]/button")).click();
-		});
-		
-		driver.switchTo().defaultContent(); // we are now outside both frames
-		driver.switchTo().frame("url_search_iframe");
-		
-		run("Change to list mode if not in it", () -> {
-			WebElement resultsGridCurrentType = driver.findElementByXPath("//*[@id=\"urlSearchContainer\"]/div[1]/button[3]");
-			
-			LocalizedString listText = new LocalizedString().pt("Lista").en("List");
+    private void test(String url, Locale locale, List<String> visibleVersions, List<String> invisibleVersions) {
 
-			if (!resultsGridCurrentType.getText().contains(listText.apply(locale))) {
-				driver.findElementByXPath("//*[@id=\"layoutTV\"]/button").click();
-			}
-		});
-		
-		run("Check specific timestamp should exist", () -> {
-			visibleVersions.stream().forEach(version -> {
-				WebElement e = driver.findElement(By.id(version));
-				assertNotNull(String.format("The timestamp %s for %s shoud be presented", version, url), e);
-			});
-		});
+        LocaleUtils.changeLanguageTo(this, locale);
 
-		run("Check specific timestamp should not exist", () -> {
-			invisibleVersions.stream().forEach(version -> {
-				String msg = String.format("The timestamp %s for %s shoud not exist", version, url);
+        run("Search url", () -> {
+            driver.findElement(By.id("submit-search-input")).clear();
+            driver.findElement(By.id("submit-search-input")).sendKeys(url);
+            driver.findElement(By.id("submit-search")).click();
+        });
 
-				appendError(msg, () -> new WebDriverWait(driver, 20)
-						.until(ExpectedConditions.invisibilityOfElementLocated(By.id(version))));
-			});
-		});
-	}
+        LocalizedString listText = new LocalizedString().pt("Lista").en("List");
+
+        assertThat("List button is available",
+            driver.findElement(By.id("replay-list-button")).getText(), containsString(listText.apply(locale)));
+
+        run("Change to list mode if not in it", () -> {
+            WebElement resultsGridCurrentType = driver.findElementByXPath("//*[@id=\"replay-list\"]/input[5]");
+            if (!resultsGridCurrentType.getAttribute("value").contains("list")) {
+                driver.findElementById("replay-list-button").click();
+            }
+        });
+
+        run("Check specific timestamp should exist", () -> {
+            visibleVersions.stream().forEach(version -> {
+                WebElement e = driver.findElement(By.id(version));
+                assertNotNull(String.format("The timestamp %s for %s shoud be presented", version, url), e);
+            });
+        });
+
+        run("Check specific timestamp should not exist", () -> {
+            invisibleVersions.stream().forEach(version -> {
+                String msg = String.format("The timestamp %s for %s shoud not exist", version, url);
+
+                appendError(msg, () -> new WebDriverWait(driver, 20)
+                        .until(ExpectedConditions.invisibilityOfElementLocated(By.id(version))));
+            });
+        });
+    }
 
 }
