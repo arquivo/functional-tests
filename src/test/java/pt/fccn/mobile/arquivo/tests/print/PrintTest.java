a21890086962decc774cf13bb0de610081d0a4b6
Vítor Gouveia <vitorgg@gmail.com>
2021-11-19 15:55:10 +0000

    Updated printtest preprod

--- src/test/java/pt/fccn/mobile/arquivo/tests/print/PrintTest.java
+++ src/test/java/pt/fccn/mobile/arquivo/tests/print/PrintTest.java
@@ -39,7 +39,7 @@ public class PrintTest extends AppendableErrorsBaseTest {
 
         String md5 = getMd5(bytes);
 
-        assertEquals("Verify print md5sum", "f7a0fbcf7268f98a7b38840a09abf479", md5);
+        assertEquals("Verify print md5sum", "9c7f323ed8fc3e2c64d37d4225492abb", md5);
     }
 
     private byte[] print(String screenshotUrlStr) {
