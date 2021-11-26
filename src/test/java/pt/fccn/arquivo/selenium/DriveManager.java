a9df165044e88a9768c42c2e2a4ee2069a8908ca
Vasco Rato <vasco.rato@fccn.pt>
2021-11-26 17:54:46 +0000

    Added mobile support to DriverManager

--- src/test/java/pt/fccn/arquivo/selenium/DriveManager.java
+++ src/test/java/pt/fccn/arquivo/selenium/DriveManager.java
@@ -4,12 +4,20 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Map;
 
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariOptions;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 
 public class DriveManager {
 
@@ -47,36 +55,60 @@ public class DriveManager {
         switch (browser) {
             case CHROME:
                 ChromeOptions chromeOptions = new ChromeOptions();
-                chromeOptions.setCapability("sauce:options", sauceOptions);
                 capabilities = (MutableCapabilities) chromeOptions;
                 break;
             case FIREFOX:
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
-                firefoxOptions.setCapability("sauce:options", sauceOptions);
                 capabilities = (MutableCapabilities) firefoxOptions;
                 break;
             case MICROSOFTEDGE:
                 EdgeOptions edgeOptions = new EdgeOptions();
-                edgeOptions.setCapability("sauce:options", sauceOptions);
                 capabilities = (MutableCapabilities) edgeOptions;
                 break;
             case SAFARI:
                 SafariOptions safariOptions = new SafariOptions();
-                safariOptions.setCapability("sauce:options", sauceOptions);
                 capabilities = (MutableCapabilities) safariOptions;
                 break;
             default:
-                throw new Exception("Driver not supported");
+                capabilities = new MutableCapabilities();
+                capabilities.setCapability(CapabilityType.BROWSER_NAME, browserName);
         }
 
-        capabilities.setCapability("platformName", platformName);
-        capabilities.setCapability("browserVersion", browserVersion);
+        capabilities.setCapability(CapabilityType.PLATFORM_NAME, platformName);
+        capabilities.setCapability(CapabilityType.BROWSER_VERSION, browserVersion);
+        capabilities.setCapability("sauce:options", sauceOptions);
 
         System.out.println("Web driver configurations: browser[" + browserName +
             "], browser version[" + browserVersion +
             "], platform name[" + platformName +
             "], sauce options[" + sauceOptions.toString() + "]"
         );
+        System.out.println("Capabilities: " + capabilities.toString());
         return new RemoteWebDriver(buildUrl(), capabilities);
+    }
+
+    public RemoteWebDriver getMobileDriver(String PlatformName, String browserName, String deviceName, String platformVersion, Map<String, Object> sauceOptions) throws Exception {
+
+        MutableCapabilities capabilities = new MutableCapabilities();
+        capabilities.setCapability(CapabilityType.PLATFORM_NAME,PlatformName);
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, browserName);
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, deviceName);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, platformVersion);
+        capabilities.setCapability("sauce:options", sauceOptions);
+
+        System.out.println("moibile Web driver configurations: browser[" + browserName +
+            "], device name[" + deviceName +
+            "], platform version[" + platformVersion +
+            "], sauce options[" + sauceOptions.toString() + "]"
+        );
+        System.out.println("Capabilities: " + capabilities.toString());
+
+        if(PlatformName.equals("iOS")){
+            return new IOSDriver<>(buildUrl(), capabilities);
+        } else if (PlatformName.equals("Android")){
+            return new AndroidDriver<>(buildUrl(), capabilities);
+        } else {
+            return new AppiumDriver<>(buildUrl(), capabilities);
+        }
     }
 }
