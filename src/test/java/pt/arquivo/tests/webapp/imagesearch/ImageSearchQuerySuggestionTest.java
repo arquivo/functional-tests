da7f11ba6efaaacf3628c3946d0dd5260f358c6b
Vasco Rato <vasco.rato@fccn.pt>
2022-10-20 16:23:11 +0100

    added imagesearch tests

---
+++ src/test/java/pt/arquivo/tests/webapp/imagesearch/ImageSearchQuerySuggestionTest.java
@@ -0,0 +1,41 @@
+package pt.arquivo.tests.webapp.imagesearch;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import pt.fccn.arquivo.selenium.Retry;
+import pt.fccn.arquivo.selenium.WebDriverTestBaseParallel;
+
+/**
+*
+* @author Ivo Branco <ivo.branco@fccn.pt>
+*
+*/
+public class ImageSearchQuerySuggestionTest extends WebDriverTestBaseParallel {
+
+	public ImageSearchQuerySuggestionTest(String os, String version, String browser, String deviceName,
+			String deviceOrientation) {
+		super(os, version, browser, deviceName, deviceOrientation);
+	}
+
+	@Test
+	@Retry
+	public void imageSearchQuerySuggestionTest() {
+		run("Search with testre", () -> {
+			driver.findElement(By.id("submit-search-input")).clear();
+			driver.findElement(By.id("submit-search-input")).sendKeys("amazoncouk");
+			driver.findElement(By.id("submit-search")).click();
+		});
+
+		run("Search images instead of text", () -> {
+			waitUntilElementIsVisibleAndGet(By.cssSelector("#search-form-images button")).click();
+		});
+
+		assertThat("Verify if a suggestion is presented",
+				driver.findElement(By.cssSelector("#term-suggested a")).getText(), containsString("amazon.co.uk"));
+	}
+
+}
