9b20c7888eba0086e9f1ea0c48a7c0c2dcc3af12
Vasco Rato <vasco.rato@fccn.pt>
2022-10-24 15:20:58 +0100

    added remaining url tests

--- src/test/java/pt/arquivo/tests/webapp/utils/CustomConditions.java
+++ src/test/java/pt/arquivo/tests/webapp/utils/CustomConditions.java
@@ -1,8 +1,10 @@
 package pt.arquivo.tests.webapp.utils;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class CustomConditions {
 
@@ -16,6 +18,23 @@ public class CustomConditions {
                 d.switchTo().defaultContent();
                 JavascriptExecutor j = (JavascriptExecutor) d;
                 return Boolean.valueOf(j.executeScript("return window.location.href").toString().contains(fraction));
+            }
+        });
+    }
+
+
+    /**
+     * Workaround for some webdrivers that break when we try to use <code>ExpectedConditions.invisibilityOfElementLocated()</code> on elements that aren't present. 
+     */ 
+    public static ExpectedCondition<Boolean> invisibilityOfElementLocatedById(String id) {
+        return (new ExpectedCondition <Boolean>() {
+            @Override
+            public Boolean apply(WebDriver d) {
+                if(d.findElements(By.cssSelector("head, #"+id)).stream().count() == 1){
+                    return Boolean.valueOf(true);
+                } else {
+                    return Boolean.valueOf(d.findElement(By.id(id)).isDisplayed());
+                }
             }
         });
     }
