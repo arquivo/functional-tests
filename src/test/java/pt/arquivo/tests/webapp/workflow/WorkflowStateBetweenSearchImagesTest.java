5db21e349bf353347dc94da422595d62cec95ac9
Vasco Rato <vasco.rato@fccn.pt>
2022-10-28 11:51:30 +0100

    Adapted workflow tests to safari. Also fixed typo.

--- src/test/java/pt/arquivo/tests/webapp/workflow/WorkflowStateBetweenSearchImagesTest.java
+++ src/test/java/pt/arquivo/tests/webapp/workflow/WorkflowStateBetweenSearchImagesTest.java
@@ -2,11 +2,16 @@ package pt.arquivo.tests.webapp.workflow;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.Duration;
+
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import pt.fccn.arquivo.selenium.Retry;
 import pt.fccn.arquivo.selenium.WebDriverTestBaseParallel;
@@ -18,31 +23,29 @@ import pt.arquivo.tests.webapp.utils.DatePicker;
  *
  */
 
-public class WorkflowStateBetweenSeachImagesTest extends WebDriverTestBaseParallel {
+public class WorkflowStateBetweenSearchImagesTest extends WebDriverTestBaseParallel {
 
-    public WorkflowStateBetweenSeachImagesTest(String os, String version, String browser, String deviceName,
+    public WorkflowStateBetweenSearchImagesTest(String os, String version, String browser, String deviceName,
             String deviceOrientation, String automationName) {
         super(os, version, browser, deviceName, deviceOrientation, automationName);
     }
 
     @Test
     @Retry
-    public void stateBetweenSeachImagesTest() throws Exception {
+    public void stateBetweenSearchImagesTest() throws Exception {
         run("Search FCCN term", () -> {
-            driver.findElement(By.id("submit-search-input")).clear();
-            driver.findElement(By.id("submit-search-input")).sendKeys("fccn");
-            driver.findElement(By.id("submit-search")).click();
+            waitUntilElementIsVisibleAndGet(By.id("submit-search-input")).clear();
+            waitUntilElementIsVisibleAndGet(By.id("submit-search-input")).sendKeys("fccn");
+            waitUntilElementIsVisibleAndGet(By.id("submit-search")).click();
         });
 
-        run("Click Image Button", () -> {
-            driver.findElement(By.id("search-form-images")).click();
-        });
+        run("Click Image Button", () -> new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.visibilityOfElementLocated(By.id("search-form-images"))).click());
 
         run("Set start date to 20 June 1997", () -> DatePicker.setStartDatePicker(driver, "20/06/1997"));
 
         run("Set end date to 1 January 2014", () -> DatePicker.setEndDatePicker(driver, "01/01/2014"));
 
-        run("Click on search button", () -> driver.findElement(By.id("submit-search")).click());
+        run("Click on search button", () -> waitUntilElementIsVisibleAndGet(By.id("submit-search")).click());
 
         run("Go to the next page", () -> {
             waitUntilElementIsVisibleAndGet(By.xpath("//*[@id=\"images-results\"]/section/form/button")).click();
@@ -51,44 +54,44 @@ public class WorkflowStateBetweenSeachImagesTest extends WebDriverTestBaseParall
         waitUntilElementIsVisibleAndGet(By.id("images-results"));
 
         appendError(() -> assertEquals("Check if fccn is in search box on second page", "fccn",
-                driver.findElement(By.id("submit-search-input")).getAttribute("value").trim()));
+                waitUntilElementIsVisibleAndGet(By.id("submit-search-input")).getAttribute("value").trim()));
 
         System.out.println("Current url: " + driver.getCurrentUrl());
 
         appendError(() -> assertEquals("Check if 1977 is in the year left datepicker", "1997",
-                driver.findElement(By.id("start-year")).getAttribute("value").trim()));
+                waitUntilElementIsVisibleAndGet(By.id("start-year")).getAttribute("value").trim()));
 
         appendError(() -> assertEquals("Check if 1977 is in the year left datepicker", "20 Jun",
-                driver.findElement(By.id("start-day-month")).getAttribute("value").trim()));
+                waitUntilElementIsVisibleAndGet(By.id("start-day-month")).getAttribute("value").trim()));
 
 
         appendError(() -> assertEquals("Check if 1977 is in the year left datepicker", "2014",
-                driver.findElement(By.id("end-year")).getAttribute("value").trim()));
+                waitUntilElementIsVisibleAndGet(By.id("end-year")).getAttribute("value").trim()));
 
         appendError(() -> assertEquals("Check if 1977 is in the year left datepicker", "1 Jan",
-                driver.findElement(By.id("end-day-month")).getAttribute("value").trim()));
+                waitUntilElementIsVisibleAndGet(By.id("end-day-month")).getAttribute("value").trim()));
 
         run("Go to the previous page", () -> {
             waitUntilElementIsVisibleAndGet(By.xpath("//*[@id=\"images-results\"]/section/form[1]/button")).click();
         });
 
         appendError(() -> assertEquals("Check if fccn is in search box on second page", "fccn",
-                driver.findElement(By.id("submit-search-input")).getAttribute("value").trim()));
+                waitUntilElementIsVisibleAndGet(By.id("submit-search-input")).getAttribute("value").trim()));
 
         System.out.println("Current url: " + driver.getCurrentUrl());
 
         appendError(() -> assertEquals("Check if 1977 is in the year left datepicker", "1997",
-                driver.findElement(By.id("start-year")).getAttribute("value").trim()));
+                waitUntilElementIsVisibleAndGet(By.id("start-year")).getAttribute("value").trim()));
 
         appendError(() -> assertEquals("Check if 1977 is in the year left datepicker", "20 Jun",
-                driver.findElement(By.id("start-day-month")).getAttribute("value").trim()));
+                waitUntilElementIsVisibleAndGet(By.id("start-day-month")).getAttribute("value").trim()));
 
 
         appendError(() -> assertEquals("Check if 1977 is in the year left datepicker", "2014",
-                driver.findElement(By.id("end-year")).getAttribute("value").trim()));
+                waitUntilElementIsVisibleAndGet(By.id("end-year")).getAttribute("value").trim()));
 
         appendError(() -> assertEquals("Check if 1977 is in the year left datepicker", "1 Jan",
-                driver.findElement(By.id("end-day-month")).getAttribute("value").trim()));
+                waitUntilElementIsVisibleAndGet(By.id("end-day-month")).getAttribute("value").trim()));
 
     }
 
