ff5092e200a0434ba3c9226e397f51817475066c
PedroG1515 <pedro.almeida.gomes@tecnico.ulisboa.pt>
2022-12-14 10:52:45 +0000

    Add CitationSAver

---
+++ src/test/java/pt/arquivo/tests/webapp/citationsaver/CitationSaverTest.java
@@ -0,0 +1,54 @@
+package pt.arquivo.tests.webapp.citationsaver;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+import pt.arquivo.selenium.Retry;
+import pt.arquivo.selenium.WebDriverTestBaseParallel; 
+
+/**
+ *
+ * @author Pedro Gomes <pedro.gomes@fccn.pt>
+ *
+ */
+public class CitationSaverTest extends WebDriverTestBaseParallel {
+
+    public CitationSaverTest(String platformName, String platformVersion, String browser, String browserVersion, String deviceName, String deviceOrientation, String automationName, String resolution) {
+        super(platformName, platformVersion, browser, browserVersion, deviceName, deviceOrientation, automationName, resolution);
+    }
+
+    @Test
+    @Retry
+    public void savePageNowURLNotFound() {
+        savePageNowURLNotFound("/services/citationsaver");
+    }
+    
+    private void savePageNowURLNotFound(String path) {
+
+        driver.get(this.testURL + path);
+
+
+        run("Wait for citationSaver page", () -> {
+            waitUntilElementIsVisibleAndGet(By.id("logo-citation-saver"));
+        });
+
+        appendError(() -> assertEquals("Verify slogan from CitationSaver", "Preserva citações a conteúdos online",
+            waitUntilElementIsVisibleAndGet(By.xpath("//*[@id=\"save-page-now-slogan\"]")).getText().trim()));
+        
+        appendError(() -> assertEquals("Verify text from CitationSaver", "Submeta um documento e o CitationSaver preservará as ligações nele citadas:",
+            waitUntilElementIsVisibleAndGet(By.xpath("//*[@id=\"citation-saver-main\"]/p[3]")).getText().trim()));
+
+        appendError(() -> assertEquals("Verify text from CitationSaver", "Insira o URL do documento:",
+            waitUntilElementIsVisibleAndGet(By.xpath("//*[@id=\"citation-saver-main\"]/div[1]/div[1]/form/label[1]")).getText().trim()));
+
+        run("Wait for results not found", () -> {
+            waitUntilElementIsVisibleAndGet(By.xpath("//*[@id=\"citation-saver-main\"]/div[1]/label[2]")).click();
+        });
+
+        appendError(() -> assertEquals("Verify text from CitationSaver", "Carregue um documento a partir do seu computador:",
+            waitUntilElementIsVisibleAndGet(By.xpath("//*[@id=\"citation-saver-main\"]/div[1]/div[2]/form/label[1]")).getText().trim()));
+
+    }
+
+}
