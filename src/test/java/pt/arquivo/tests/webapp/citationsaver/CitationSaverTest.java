a620d342538fc86cc099ac9008f065fb8b41d947
Vasco Rato <vasco.rato@fccn.pt>
2023-11-28 17:40:58 +0000

    fixed citation saver test for iOS

--- src/test/java/pt/arquivo/tests/webapp/citationsaver/CitationSaverTest.java
+++ src/test/java/pt/arquivo/tests/webapp/citationsaver/CitationSaverTest.java
@@ -6,6 +6,9 @@ import java.util.Map;
 
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import pt.arquivo.selenium.Retry;
 import pt.arquivo.selenium.WebDriverTestBaseParallel; 
@@ -32,20 +35,29 @@ public class CitationSaverTest extends WebDriverTestBaseParallel {
         });
 
         appendError(() -> assertEquals("Verify slogan from CitationSaver", "Preserva citações a conteúdos online",
-            waitUntilElementIsVisibleAndGet(By.xpath("//*[@id=\"citation-saver-slogan\"]")).getText().trim()));
+            waitUntilElementIsVisibleAndGet(By.id("citation-saver-slogan")).getText().trim()));
         
         appendError(() -> assertEquals("Verify text from CitationSaver", "Submeta um documento e o CitationSaver preservará as ligações nele citadas:",
             waitUntilElementIsVisibleAndGet(By.xpath("//*[@id=\"citation-saver-main\"]/p[3]")).getText().trim()));
 
         appendError(() -> assertEquals("Verify text from CitationSaver", "Insira o URL do documento:",
-            waitUntilElementIsVisibleAndGet(By.xpath("//*[@id=\"citation-saver-main\"]/div[1]/div[1]/form/label[1]")).getText().trim()));
+            waitUntilElementIsVisibleAndGet(By.cssSelector("form label[for=\"input-url-url\"]")).getText().trim()));
 
-        run("Wait for results not found", () -> {
-            waitUntilElementIsVisibleAndGet(By.xpath("//*[@id=\"citation-saver-main\"]/div[1]/label[2]")).click();
+        run("Click on the 'Ficheiros' tab", () -> {
+            Capabilities capabilities = ((RemoteWebDriver) driver).getCapabilities();
+            String platform = capabilities.getPlatformName().name();
+            if (platform.toLowerCase().equals("ios")){
+                // IOS driver is dumb and sometimes fails to click properly. So instead we use javascript to click.
+                waitUntilElementIsVisibleAndGet(By.cssSelector("label[for=\"input-file-upload\"]"));
+                ((JavascriptExecutor) driver).executeScript("document.querySelector('label[for=\"input-file-upload\"]').click()");
+                
+            } else {
+                waitUntilElementIsVisibleAndGet(By.cssSelector("label[for=\"input-file-upload\"]")).click();
+            }
         });
 
         appendError(() -> assertEquals("Verify text from CitationSaver", "Carregue um documento a partir do seu computador:",
-            waitUntilElementIsVisibleAndGet(By.xpath("//*[@id=\"citation-saver-main\"]/div[1]/div[2]/form/label[1]")).getText().trim()));
+            waitUntilElementIsVisibleAndGet(By.cssSelector("form label[for=\"input-file-file\"]")).getText().trim()));
 
     }
 
