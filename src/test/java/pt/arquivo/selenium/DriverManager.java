8780cc6cbc3bdec791c4aa07725112aece5f6aab
Vasco Rato <vasco.rato@fccn.pt>
2025-11-21 18:33:45 +0000

    upgraded selenium. Fixed mobile driver.

--- src/test/java/pt/arquivo/selenium/DriverManager.java
+++ src/test/java/pt/arquivo/selenium/DriverManager.java
@@ -17,7 +17,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.options.XCUITestOptions;
-import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.remote.AutomationName;
 
 public class DriverManager {
 
@@ -36,7 +36,7 @@ public class DriverManager {
     }
 
     public RemoteWebDriver getDriver(Map<String, String> config, Map<String, Object> sauceOptions) throws MalformedURLException {
-        if (config.containsKey(MobileCapabilityType.DEVICE_NAME)) {
+        if (config.containsKey("device")) {
             return getMobileDriver(config, sauceOptions);
         } else {
             return getDesktopDriver(config, sauceOptions);
@@ -77,39 +77,63 @@ public class DriverManager {
     }
 
     public RemoteWebDriver getMobileDriver(Map<String, String> config, Map<String, Object> sauceOptions) throws MalformedURLException {
-        switch (config.get(MobileCapabilityType.AUTOMATION_NAME)) {
-            case "XCUITest":
+       
+        String automationName = config.get("automationName"); // Use the string literal or a custom enum/constant
+
+        // We now use a base MutableCapabilities to combine all options later
+        MutableCapabilities finalCapabilities;
+
+        switch (automationName) {
+            case AutomationName.IOS_XCUI_TEST: 
                 XCUITestOptions xcuiOptions = new XCUITestOptions();
-                capabilities = xcuiOptions;
-                capabilities.setCapability("appium:nativeWebTap", true);
-                capabilities.setCapability("appium:nativeWebTapStrict", true);
+                // xcuiOptions.setNativeWebTap(true);
+                // xcuiOptions.setNativeWebTapStrict(true);
+                finalCapabilities = xcuiOptions;
                 break;
-            case "UiAutomator2":
-                capabilities = new UiAutomator2Options();
+            case AutomationName.ANDROID_UIAUTOMATOR2: 
+                UiAutomator2Options uiAutomator2Options = new UiAutomator2Options();
+                finalCapabilities = uiAutomator2Options;
                 break;
             default:
-                capabilities = new MutableCapabilities();
+                // Fallback for other/unknown drivers
+                finalCapabilities = new MutableCapabilities();
+                finalCapabilities.setCapability(CapabilityType.PLATFORM_NAME, config.get(CapabilityType.PLATFORM_NAME));
+                finalCapabilities.setCapability("automationName", automationName);
+                break;
         }
 
-        capabilities.setCapability(CapabilityType.PLATFORM_NAME, config.get(CapabilityType.PLATFORM_NAME));
-        capabilities.setCapability(CapabilityType.BROWSER_NAME, config.get(CapabilityType.BROWSER_NAME));
-        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, config.get(MobileCapabilityType.AUTOMATION_NAME));
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, config.get(MobileCapabilityType.PLATFORM_VERSION));
-        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, config.get(MobileCapabilityType.DEVICE_NAME));
-
-        if (config.containsKey(MobileCapabilityType.ORIENTATION)) {
-            capabilities.setCapability(MobileCapabilityType.ORIENTATION, config.get(MobileCapabilityType.ORIENTATION));
+        // Apply common capabilities from the config map to the final capabilities object
+        // Note: The Options classes (xcuiOptions/uiAutomator2Options) already set platformName/automationName internally.
+        // We use standard W3C keys where possible.
+        
+        if (config.containsKey(CapabilityType.BROWSER_NAME)) {
+           finalCapabilities.setCapability(CapabilityType.BROWSER_NAME, config.get(CapabilityType.BROWSER_NAME));
         }
 
-        capabilities.setCapability("sauce:options", sauceOptions);
+        if (config.containsKey("platformVersion")) {
+           // This will automatically be vendor-prefixed as 'appium:platformVersion' if needed
+           finalCapabilities.setCapability("appium:platformVersion", config.get("platformVersion"));
+        }
+        
+        if (config.containsKey("device")) {
+            finalCapabilities.setCapability("appium:deviceName", config.get("device"));
+        }
+
+        if (config.containsKey("deviceOrientation")) {
+            finalCapabilities.setCapability("appium:orientation", config.get("deviceOrientation"));
+        }
+        
+        // Add sauce:options object as a standard capability
+        finalCapabilities.setCapability("sauce:options", sauceOptions);
 
         String platform = config.get(CapabilityType.PLATFORM_NAME);
         if ("iOS".equalsIgnoreCase(platform)) {
-            return new IOSDriver(buildUrl(), capabilities);
+            // Pass the options (which are MutableCapabilities) directly to the driver constructor
+            return new IOSDriver(buildUrl(), finalCapabilities);
         } else if ("Android".equalsIgnoreCase(platform)) {
-            return new AndroidDriver(buildUrl(), capabilities);
+            return new AndroidDriver(buildUrl(), finalCapabilities);
         } else {
-            return new AppiumDriver(buildUrl(), capabilities);
+            return new AppiumDriver(buildUrl(), finalCapabilities);
         }
     }
 }
